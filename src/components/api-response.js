a4d3f9c7e8ffdc61ea85996f7a795cc272c593cd
Mrinmoy <mrin9@yahoo.com>
2021-03-20 21:32:27 -0700

    improved response header rendering alignment

--- src/components/api-response.js
+++ src/components/api-response.js
@@ -124,7 +124,7 @@ export default class ApiResponse extends LitElement {
         allMimeResp[mimeResp] = {
           description: this.responses[statusCode].description,
           examples: respExamples,
-          selectedExample: respExamples[0] ? respExamples[0].exampleId : '',
+          selectedExample: respExamples[0]?.exampleId || '',
           schemaTree,
         };
       }
@@ -204,18 +204,17 @@ export default class ApiResponse extends LitElement {
   responseHeaderListTemplate(respHeaders) {
     return html`
       <div style="padding:16px 0 8px 0" class="resp-headers small-font-size upper bold-text">Response Headers:</div> 
-      <table style="padding-bottom:16px;" class='small-font-size mono-font'>
+      <table style="border-collapse: collapse; margin-bottom:16px; border:1px solid var(--border-color); border-radius: var(--border-radius)" class="small-font-size mono-font">
         ${respHeaders.map((v) => html`
           <tr>
-            <td style="vertical-align: top;"> ${v.name}</td> 
-            <td style="vertical-align: top; padding:0 5px;"> 
-              ${v.schema.type ? v.schema.type : ''}
+            <td style="padding:8px; vertical-align: baseline; min-width:120px; border-top: 1px solid var(--light-border-color); text-overflow: ellipsis;">
+              ${v.name || ''}
             </td> 
             <td style="vertical-align: top;">
               <div class="regular-font m-markdown m-markdown-small">${unsafeHTML(marked(v.description || ''))}</div>
             </td>
-            <td style="vertical-align: top;">
-              ${v.schema.example ? v.schema.example : ''}
+            <td style="padding:8px; vertical-align: baseline; border-top: 1px solid var(--light-border-color); text-overflow: ellipsis;">
+              ${v.schema.example || ''}
             </td>
           </tr>
         `)}
