2a096ae6e99f34812579940338e99104f8c33614
Warren Parad <wparad@gmail.com>
2021-03-26 20:52:39 +0100

    Fix array display syntax.

--- src/utils/schema-utils.js
+++ src/utils/schema-utils.js
@@ -469,9 +469,9 @@ export function schemaInObjectNotation(schema, obj, level = 0, suffix = '') {
       } else if (v === 'array' && typeof subSchema.items?.type === 'string' && subSchema.items?.type.match(/integer|number|string|null|boolean/g)) {
         // Array with primitive types should also be treated as primitive type
         if (subSchema.items.type === 'string' && subSchema.items.format) {
-          primitiveType.push(`[${subSchema.items.format}]`);
+          primitiveType.push(`${subSchema.items.format}[]`);
         } else {
-          primitiveType.push(`[${subSchema.items.type}]`);
+          primitiveType.push(`${subSchema.items.type}[]`);
         }
       } else {
         complexTypes.push(v);
