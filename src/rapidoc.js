6b5f34cfc5920efd6e1281c912f82e91e5782e8a
Warren Parad <wparad@gmail.com>
2021-03-27 12:46:48 +0100

    Fix updating request headers.

--- src/rapidoc.js
+++ src/rapidoc.js
@@ -588,7 +588,7 @@ export default class RapiDoc extends LitElement {
         this.loadSpec(specObj);
         this.shadowRoot.getElementById('spec-url').value = '';
       } catch (err) {
-        console.error('RapiDoc: Unable to read or parse json'); // eslint-disable-line no-console
+        console.error('OpenAPI Explorer: Unable to read or parse json'); // eslint-disable-line no-console
       }
     };
     // Read the Text file
@@ -662,7 +662,7 @@ export default class RapiDoc extends LitElement {
       this.loading = false;
       this.loadFailed = true;
       this.resolvedSpec = null;
-      console.error(`RapiDoc: Unable to resolve the API spec..  ${err.message}`); // eslint-disable-line no-console
+      console.error(`OpenAPI Explorer: Unable to resolve the API spec..  ${err.message}`); // eslint-disable-line no-console
     }
   }
 
