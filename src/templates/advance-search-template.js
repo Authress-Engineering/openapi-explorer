b54e9809ea78b1bd17f1660c0e67cf30c7b11867
Warren Parad <wparad@gmail.com>
2021-04-06 12:48:06 +0200

    Fix advanced search dialog box colors. fix #7

--- src/templates/advance-search-template.js
+++ src/templates/advance-search-template.js
@@ -1,71 +1,81 @@
 import { html } from 'lit-element';
-import '../components/dialog-box';
 
 /* eslint-disable indent */
 export default function searchByPropertiesModalTemplate() {
-  document.addEventListener('close', () => { this.showAdvancedSearchDialog = false; });
-  document.addEventListener('open', this.onOpenSearchDialog);
+  const keyDownEventListenerAdvancedSearch = (e) => {
+    if ((e.detail?.code || e.code) === 'Escape') {
+      this.showAdvancedSearchDialog = false;
+    }
+  };
+
+  const closeAdvancedSearchDialog = () => {
+    // Trigger the event to force it to be removed from the DOM
+    document.dispatchEvent(new CustomEvent('keydown', { detail: { code: 'Escape' } }));
+    document.removeEventListener('keydown', keyDownEventListenerAdvancedSearch, { once: true });
+  };
+
+  document.addEventListener('keydown', keyDownEventListenerAdvancedSearch, { once: true });
 
   return html`
-    <dialog-box 
-      heading="Advanced Search" 
-      show="${(!!this.showAdvancedSearchDialog)}"
-    >
-      <span class="advanced-search-options">
-        <input
-          style="width:100%; padding-right:20px;"
-          type="text"
-          part="textbox textbox-search-dialog"
-          placeholder="search text..."
-          spellcheck="false"
-          @keyup = "${(e) => this.onAdvancedSearch(e, 400)}"
-        >
-        <div style="display:flex; margin:8px 0 24px;">
-          <div>
-            <input style="cursor:pointer;" type="checkbox" part="checkbox checkbox-search-dialog" id="search-api-path" checked @change = "${(e) => this.onAdvancedSearch(e, 0)}">
-            <label for="search-api-path" style="cursor:pointer;"> API Path </label>
+  ${this.showAdvancedSearchDialog
+    ? html`
+      <div class="dialog-box-overlay">
+        <div class="dialog-box">
+          <header class="dialog-box-header">
+            <span class="dialog-box-title">Advanced Search</span>
+            <button class="m-btn thin-border" @click="${() => { closeAdvancedSearchDialog(); }}" part="btn btn-outline">&times;</button>
+          </header>
+          <div id="advanced-search-modal" class="dialog-box-content">
+            <span class="advanced-search-options">
+              <div class="advanced-search-dialog-input">
+                <input id="advanced-search-dialog-input" type="text" part="textbox textbox-search-dialog" placeholder="search text..." spellcheck="false"
+                  @keyup = "${(e) => this.onAdvancedSearch(e, 400)}">
+              </div>
+              <div class="advanced-search-locations">
+                <div>
+                  <input style="cursor:pointer;" type="checkbox" part="checkbox checkbox-search-dialog" id="search-api-path" checked @change = "${(e) => this.onAdvancedSearch(e, 0)}">
+                  <label style="cursor:pointer;" for="search-api-path"> API Path </label>
+                </div>
+                <div>
+                  <input style="cursor:pointer;" type="checkbox" part="checkbox checkbox-search-dialog" id="search-api-descr" checked @change = "${(e) => this.onAdvancedSearch(e, 0)}">
+                  <label style="cursor:pointer;" for="search-api-descr"> API Description </label>
+                </div>
+                <div>
+                  <input style="cursor:pointer;" type="checkbox" part="checkbox checkbox-search-dialog" id="search-api-params" @change = "${(e) => this.onAdvancedSearch(e, 0)}">
+                  <label style="cursor:pointer;" for="search-api-params"> Request Parameters </label>
+                </div>
+                <div>
+                  <input style="cursor:pointer;" type="checkbox" part="checkbox checkbox-search-dialog" id="search-api-request-body" @change = "${(e) => this.onAdvancedSearch(e, 0)}">
+                  <label style="cursor:pointer;" for="search-api-request-body"> Request Body </label>
+                </div>
+                <div>
+                  <input style="cursor:pointer;" type="checkbox" part="checkbox checkbox-search-dialog" id="search-api-resp-descr" @change = "${(e) => this.onAdvancedSearch(e, 0)}">
+                  <label style="cursor:pointer;" for="search-api-resp-descr"> Response Description </label>
+                </div>
+              </div>
+            </span>
+            
+            <div class="advanced-search-results">
+              ${this.advancedSearchMatches?.map((path) => html`
+                <div
+                  class="mono-font small-font-size hover-bg" tabindex = '0'
+                  style='padding: 5px; cursor: pointer; border-bottom: 1px solid var(--light-border-color); ${path.deprecated ? 'filter:opacity(0.5);' : ''}' 
+                  data-content-id='${path.elementId}'
+                  @click="${(e) => {
+                    this.matchPaths = ''; // clear quick filter if applied
+                    closeAdvancedSearchDialog(); // Hide Search Dialog
+                    this.requestUpdate();
+                    this.scrollToEventTarget(e, true);
+                  }}"> 
+                  <span class="upper bold-text method-fg ${path.method}">${path.method}</span> 
+                  <span>${path.path}</span> - <span class="regular-font gray-text">${path.summary}</span>
+                </div>
+              `)}
             </div>
-          <div style="margin-left: 16px;">
-            <input style="cursor:pointer;" type="checkbox" part="checkbox checkbox-search-dialog" id="search-api-descr" checked @change = "${(e) => this.onAdvancedSearch(e, 0)}">
-            <label style="cursor:pointer;" for="search-api-descr"> API Description </label>
-          </div>
-          <div style="margin-left: 16px;">
-            <input style="cursor:pointer;" type="checkbox" part="checkbox checkbox-search-dialog" id="search-api-params" @change = "${(e) => this.onAdvancedSearch(e, 0)}">
-            <label style="cursor:pointer;" for="search-api-params"> API Parameters </label>
-          </div>
-          <div style="margin-left: 16px;">
-            <input style="cursor:pointer;" type="checkbox" part="checkbox checkbox-search-dialog" id="search-api-request-body" @change = "${(e) => this.onAdvancedSearch(e, 0)}">
-            <label style="cursor:pointer;" for="search-api-request-body"> Request Body Parameters </label>
-          </div>
-          <div style="margin-left: 16px;">
-            <input style="cursor:pointer;" type="checkbox" part="checkbox checkbox-search-dialog" id="search-api-resp-descr" @change = "${(e) => this.onAdvancedSearch(e, 0)}">
-            <label style="cursor:pointer;" for="search-api-resp-descr"> Response Description </label>
           </div>
         </div>
-      </span>
-      
-      ${this.advancedSearchMatches?.map((path) => html`
-      <div
-        class="mono-font small-font-size hover-bg"
-        style='padding: 5px; cursor: pointer; border-bottom: 1px solid var(--light-border-color); ${path.deprecated ? 'filter:opacity(0.5);' : ''}' 
-        data-content-id='${path.elementId}'
-        tabindex = '0'
-        @click="${
-          (e) => {
-            this.matchPaths = ''; // clear quick filter if applied
-            this.showAdvancedSearchDialog = false; // Hide Search Dialog
-            this.requestUpdate();
-            this.scrollToEventTarget(e, true);
-          }
-        }"
-      > 
-        <span class="upper bold-text method-fg ${path.method}">${path.method}</span> 
-        <span>${path.path}</span>
-        <span class="regular-font gray-text">${path.summary}</span>
-      </div>
-    `)
-    }
-    </dialog-box>
-  `;
+      </div>`
+    : ''
+  }`;
 }
 /* eslint-enable indent */
