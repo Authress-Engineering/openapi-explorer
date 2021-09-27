afd98b152d2c722df4aec19c22a9e4161b590331
Warren Parad <wparad@gmail.com>
2021-09-27 20:54:03 +0200

    Remove unnecessary space before token in authorization header.

--- src/templates/security-scheme-template.js
+++ src/templates/security-scheme-template.js
@@ -42,7 +42,8 @@ function onClearAllApiKeys() {
 // Updates the OAuth Access Token (API key), so it reflects in UI and gets used in TRY calls
 function updateOAuthKey(apiKeyId, tokenType = 'Bearer', accessToken) {
   const securityObj = this.resolvedSpec.securitySchemes.find((v) => (v.apiKeyId === apiKeyId));
-  securityObj.finalKeyValue = `${(tokenType.toLowerCase() === 'bearer' ? 'Bearer' : (tokenType.toLowerCase() === 'mac' ? 'MAC' : tokenType))} ${accessToken}`;
+  const tokenPrefix = { bearer: 'Bearer', mac: 'MAC' }[tokenType.toLowerCase()] || tokenType;
+  securityObj.finalKeyValue = `${tokenPrefix}${tokenPrefix ? ' ' : ''}${accessToken}`;
   this.requestUpdate();
 }
 
