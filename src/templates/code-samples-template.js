6f27d6a634aeab43c38501a71000eee81891cc1f
Warren Parad <gpg@warrenparad.net>
2022-10-01 11:57:48 +0200

    Fix padding in code samples.

--- src/templates/code-samples-template.js
+++ src/templates/code-samples-template.js
@@ -22,15 +22,17 @@ export default function codeSamplesTemplate(xCodeSamples) {
     <div class="tab-buttons row" style="width:100; overflow">
       ${xCodeSamples.map((v, i) => html`<button class="tab-btn ${i === 0 ? 'active' : ''}" data-tab = '${v.lang}${i}'> ${v.label || v.lang} </button>`)}
     </div>
-    ${xCodeSamples.map((v, i) => html`
-      <div class="tab-content m-markdown code-sample-wrapper" style= "display:${i === 0 ? 'block' : 'none'}" data-tab = '${v.lang}${i}'>
-        <button class="m-btn outline-primary toolbar-copy-btn" @click='${(e) => { copyToClipboard(v.source, e); }}'>${getI18nText('operations.copy')}</button>
-        <pre>
-          <code>
-            ${Prism.languages[v.lang && v.lang.toLowerCase()] ? unsafeHTML(Prism.highlight(v.source, Prism.languages[v.lang && v.lang.toLowerCase()], v.lang && v.lang.toLowerCase())) : v.source}
-          </code>
-        </pre>
-      </div>`)
+    ${xCodeSamples.map((v, i) => {
+      const paddingToRemove = Math.min(...v.source.split('\n').slice(1).map(l => l.match(/^(\s+).*$/)?.[1].length).filter(l => typeof l !== 'undefined'));
+      const sanitizedSource = v.source.split('\n').map(s => s.substring(0, paddingToRemove).match(/^\s+$/) ? s.substring(paddingToRemove) : s);
+      const fullSource = sanitizedSource.join('\n');
+      return html`
+        <div class="tab-content m-markdown code-sample-wrapper" style= "display:${i === 0 ? 'block' : 'none'}" data-tab = '${v.lang}${i}'>
+          <button class="m-btn outline-primary toolbar-copy-btn" @click='${(e) => { copyToClipboard(v.source, e); }}'>${getI18nText('operations.copy')}</button>
+          <pre><code>${Prism.languages[v.lang?.toLowerCase()] ? unsafeHTML(Prism.highlight(fullSource, Prism.languages[v.lang?.toLowerCase()], v.lang?.toLowerCase())) : fullSource}
+          </code></pre>
+        </div>`;
+    })
     }
   </section>`;
 }
