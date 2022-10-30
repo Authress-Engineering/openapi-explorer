8b14ff4d2527e32ec4f523a1dfe31c2caa131195
Warren Parad <gpg@warrenparad.net>
2022-10-30 11:14:52 +0100

    Improve path text wrapping.

--- src/templates/endpoint-template.js
+++ src/templates/endpoint-template.js
@@ -56,7 +56,7 @@ function endpointHeadTemplate(path) {
   <summary @click="${(e) => { toggleExpand.call(this, path, e); }}" class='endpoint-head ${path.method} ${path.expanded ? 'expanded' : 'collapsed'}'>
     <div class="method ${path.method}"> ${path.method} </div> 
     <div class="path"> 
-      ${path.path.split('/').map(p => html`<span>${p}/</span>`)} 
+      ${path.path.split('/').filter(t => t.trim()).map(t => html`<span>/${t}</span>`)} 
       ${path.isWebhook ? html`<span style="color:var(--primary-color)"> (Webhook) </span>` : ''}
     </div>
     ${path.deprecated
