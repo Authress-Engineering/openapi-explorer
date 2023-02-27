5a961ab088f4b93f16c4ef685f1871e98d657e3b
Warren Parad <gpg@warrenparad.net>
2023-02-27 13:51:10 +0100

    Fix no-select on schema-table.

--- src/styles/schema-styles.js
+++ src/styles/schema-styles.js
@@ -4,6 +4,15 @@ export default css`
 
 *, *:before, *:after { box-sizing: border-box; }
 
+.no-select {
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+}
+
 .tr {
   display: flex;
   flex: none;
