8389fab6efb0f0fa960310e0df1fd7dadf3674e1
Warren Parad <gpg@warrenparad.net>
2023-08-29 17:34:35 +0200

    Add explicit extension to oauth handler imports.

--- src/openapi-explorer.js
+++ src/openapi-explorer.js
@@ -827,5 +827,5 @@ export default class OpenApiExplorer extends LitElement {
 if (!customElements.get('openapi-explorer')) {
   customElements.define('openapi-explorer', OpenApiExplorer);
 }
-import './openapi-explorer-oauth-handler';
+import './openapi-explorer-oauth-handler.js';
 
