162eacca16d881245df359a9f45151697b67cf6d
Warren Parad <gpg@warrenparad.net>
2023-09-20 13:04:11 +0200

    initial commit

---
+++ src/styles/font-styles.js
@@ -0,0 +1,100 @@
+import vars from './vars';
+import {html} from 'lit-element'; 
+
+export default html`
+<style>
+    .regular-font{font-family:${vars.font.regular};}
+    .mono-font{font-family:${vars.font.mono};}
+
+    h1{ font-family:${vars.font.regular}; font-size:26px; font-weight:200; letter-spacing:normal; }
+    h2{ font-family:${vars.font.regular}; font-size:24px; font-weight:200; letter-spacing:normal; }
+    h3{ font-family:${vars.font.regular}; font-size:22px; font-weight:200; letter-spacing:normal; }
+    h4{ font-family:${vars.font.regular}; font-size:18px; font-weight:200; letter-spacing:normal; }
+    h5{ font-family:${vars.font.regular}; font-size:16px; font-weight:200; letter-spacing:normal; }
+    h6{ font-family:${vars.font.regular}; font-size:14px; font-weight:200; letter-spacing:normal; }
+
+    h1,h2,h3,h4,h5,h5{
+      margin-block-end: 0.2em;
+    }
+    p{margin-block-start: 0.5em;}
+    code,
+    pre{
+      font-family: ${vars.font.mono};
+    }
+
+    /* Text */
+    .m-text{vertical-align: middle;}
+    .m-text.small{font-size: 12px;line-height: 12px;}
+    .m-text.gray{color: var(--light-fg)}
+    .m-text.primary{color: var(--primary-color); }
+    .m-text.bold{font-weight:bold;}
+    .m-text.bottom-margin{ margin-bottom:4px}
+    
+    /* Sub Title */
+    .m-sub-title {
+      font-size:20px;
+      white-space:nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    .m-sub-title.m-1{
+      font-size: 16px;
+      font-weight:bold;
+      margin: 20px 0 4px 0;
+    }
+
+    .m-sub-title.m-2{
+      font-size: 14px;
+      font-weight:bold;
+      margin: 16px 0 4px 0;
+    }
+
+    .m-sub-title.m-3 {
+        font-size: 12px;
+        font-weight:bold;
+        margin: 12px 0 4px 0;
+    }
+
+    /* Markdown */
+    .m-markdown p:only-child{
+        color:var(--light-fg);
+        font-size:12px;
+        line-height:normal;
+        margin-top:0;
+    }
+    .m-markdown p{
+        line-height: 16px;
+    }
+    .m-markdown code{
+        background-color: rgba(0, 0, 0, 0.02);
+        padding: 0px 6px;
+        border: 1px solid var(--light-border-color);
+        border-radius: 3px;
+        color: var(--code-fg);
+        font-size: 12px;
+    }
+
+    .m-markdown pre{
+        white-space: pre-wrap;
+        background-color: var(--reverse-code-bg);
+        color: var(--reverse-code-fg);
+        padding: 12px 14px 15px 14px;
+        overflow-x: auto;
+        line-height: normal;
+        border-radius: 2px;
+        border: 1px solid rgba(38, 50, 56, 0.1);
+    }
+    .m-markdown pre code {
+        border:none;
+        background-color:transparent;
+        color:var(--reverse-code-fg);
+    }
+    .m-markdown ul,
+    .m-markdown ol{
+        padding-inline-start:30px
+    }
+    .m-markdown li{
+        line-height: 1.2em;
+    }
+</style>`
---
+++ src/styles/input-styles.js
@@ -0,0 +1,114 @@
+import vars from './vars';
+import {html} from 'lit-element'; 
+
+export default html`
+<style>
+input, select, button {
+  font-family: ${vars.font.regular};
+  font-weight:400;
+}
+
+/* Button */
+.m-btn{
+  border-radius: ${vars.border.radius};
+  font-weight: 600;
+  display: inline-block;
+  padding: 6px 16px;
+  font-size: 12px;
+  outline: 0;
+  line-height: 1;
+  text-align: center;
+  white-space: nowrap;
+  background-color:var(--primary-color);
+  color:var(--primary-text);
+  border: 1px solid var(--primary-color);
+  transition: background-color 0.2s;
+  user-select: none;
+  cursor: pointer;
+}
+
+.m-btn.large{padding:8px 14px;}
+.m-btn.small{padding:5px 12px;}
+.m-btn.circle{border-radius:50%;}
+.m-btn:hover{ background-color: var(--primary-color)}
+
+/* Form Inputs */
+textarea,
+input[type="text"],
+input[type="password"]{
+  border-radius:${vars.border.radius};
+  border:1px solid var(--input-border-color);
+  background:var(--input-bg);
+  color:var(--fg);
+  transition: border .2s;
+  outline: none;
+  font-size:14px;
+  height:26px;
+  padding: 0 5px;
+  box-sizing: border-box;
+}
+
+textarea.mono,
+input[type="text"].mono,
+input[type="password"].mono{
+  font-family: ${vars.font.mono}; 
+  font-size:12px;
+}
+
+input[type="text"].small,
+input[type="password"].small {
+    height:26px;
+    font-size:12px;
+}
+
+textarea::placeholder,
+input[type="text"]::placeholder,
+input[type="password"]::placeholder {
+    color: var(--placeholder-color);
+}
+
+textarea:focus,
+input[type="text"]:focus,
+input[type="password"]:focus,
+textarea:active,
+input[type="text"]:active,
+input[type="password"]:active {
+    border:1px solid var(--primary-color);
+}
+
+textarea.reverse,
+input.reverse[type="text"],
+input.reverse[type="password"]{
+  background:${vars.color.inputReverseBg};
+  color:${vars.color.inputReverseFg};
+  border:1px solid var(--primary-color); 
+}
+
+textarea.reverse:focus,
+input.reverse[type="text"]:focus,
+input.reverse[type="password"]:focus,
+textarea.reverse:active,
+input.reverse[type="text"]:active,
+input.reverse[type="password"]:active{
+    border:1px solid var(--primary-color);
+}
+
+textarea.reverse::placeholder,
+input.reverse::placeholder{
+    color: #666;
+}
+
+textarea::-webkit-scrollbar-track{
+    background-color: transparent;
+}
+  
+textarea::-webkit-scrollbar{
+    width: 8px;
+    height: 8px;
+    background-color: transparent;
+}
+ 
+textarea::-webkit-scrollbar-thumb {
+    background-color: rgba(50,50,50, .5);
+}
+</style>`
